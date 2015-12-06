4d790aebec2f3542291912d128448917f1f17ca2
XBXN1314 <530996947@qq.com>
2015-12-06 17:19:41 +0800

    Python 语法基础

--- mian.py
+++ mian.py
@@ -1,1 +1,21 @@
-print("hello bbb")
+print "hello Python!"
+print "123"
+i = 7
+print "2+3"
+
+c1 = 'sght'
+print c1
+c2 = 'It is a "dog"!'
+print c2
+
+print "hello boy\nhello boy"
+print r"hello boy\nhello boy"
+
+students = ["A", "B", "C", "D", "E"]
+print students[3]
+
+students[3] = "F"
+print students[3]
+
+students1 = ("a", "b", "c")
+print students1[2]
