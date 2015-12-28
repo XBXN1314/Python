8c62446a309de14a77d46b285f08dd008e5a105a
XBXN1314 <530996947@qq.com>
2015-12-28 21:36:28 +0800

    new

---
+++ wordgame_1.py
@@ -0,0 +1,9 @@
+print('--------------游戏开始--------------')
+temp = input ('不妨猜一猜我在想哪一个数字')
+guess = int (temp)
+if guess == 8:
+    print('恭喜你猜对了')
+else:
+    print('你答错啦')
+print('游戏结束啦')
+        
